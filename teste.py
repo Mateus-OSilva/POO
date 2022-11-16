c4454e48399a683f3eed3902c54b55dfcb0c2c88
Mateus-OSilva <mateusods20@gmail.com>
2022-11-16 20:06:26 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 
 
-    def cria_conta(numero, titular, saldo, limite):
+def cria_conta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
 
