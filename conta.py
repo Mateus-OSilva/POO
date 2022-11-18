f312922019f147b66273d007956da3b18a2e7847
Mateus-OSilva <mateusods20@gmail.com>
2022-11-18 17:50:59 -0300

    Funções deposita e saca

--- conta.py
+++ conta.py
@@ -11,3 +11,10 @@ class Conta:
 
     def extrato(self):
         print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+    
+    def saca(self, valor):
+        self.saldo -= valor
+        
