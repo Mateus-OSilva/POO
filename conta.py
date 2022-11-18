ffd23ba034e63d9d525b7a8ee8240f4e3386b03c
Mateus-OSilva <mateusods20@gmail.com>
2022-11-18 17:27:13 -0300

    a identação é importante :)

--- conta.py
+++ conta.py
@@ -3,11 +3,11 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        print ("Construindo objeto...{}".format(self))
+        print("Construindo objeto...{}".format(self))
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
 
-def extrato(self):
-    print("Saldo {} do titular {}".format(self.saldo, self.titular))
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
